ab35c505f3dbba1f034d1653cccfcc145b171658
Marlesss <60238634+Marlesss@users.noreply.github.com>
2021-04-03 15:05:15 +0300

    Freebie

---
+++ data/users.py
@@ -0,0 +1,25 @@
+import datetime
+import sqlalchemy
+import sqlalchemy.orm as orm
+from .db_session import SQLAlchemyBase
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy_serializer import SerializerMixin
+
+
+class User(SQLAlchemyBase, UserMixin):
+    __tablename__ = "users"
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    email = sqlalchemy.Column(sqlalchemy.String, index=True, nullable=False)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
+    med_card_id = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
+
+    def __repr__(self):
+        return f'User {self.id}: {self.name} {self.email}'
---
+++ main.py
@@ -0,0 +1,80 @@
+from flask import Flask, render_template, redirect, url_for, request, make_response, session, abort
+from flask import jsonify
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+import datetime
+from flask_restful import reqparse, abort, Api, Resource
+
+from Forms.register_form import RegisterForm
+from Forms.login_form import LoginForm
+from data import db_session
+from data.users import User
+
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = 'secret_key'
+app.config["PERMANENT_SESSION_LIFETIME"] = datetime.timedelta(days=365)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    session = db_session.create_session()
+    return session.query(User).get(user_id)
+
+
+def main():
+    db_session.global_init("db/almed_database.db")
+    app.run(port=8080, host="127.0.0.1")
+
+
+@app.route("/")
+def index():
+    session = db_session.create_session()
+    return render_template("index.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template("register.html", title="Регистрация", form=form,
+                                   message="Пароли не совпадают")
+        session = db_session.create_session()
+        if session.query(User).filter(User.email == form.email.data).first():
+            return render_template("register.html", title="Регистрация", form=form,
+                                   message="Эта электронная почта уже зарегистрирована!")
+        user = User(
+            email=form.email.data,
+        )
+        user.set_password(form.password.data)
+        session.add(user)
+        session.commit()
+        return redirect("/login")
+    return render_template("register.html", title="Регистрация", form=form)
+
+
+@app.route("/login", methods=['GET', "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        session = db_session.create_session()
+        user = session.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template("login.html", title="Авторизация",
+                               message="Неправильный логин или пароль", form=form)
+    return render_template("login.html", title="Авторизация", form=form)
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    main()
