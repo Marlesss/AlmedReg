c45c68b9981efb8995d0186bd050e475f3d47cb5
Marlesss <60238634+Marlesss@users.noreply.github.com>
2021-04-26 00:11:57 +0300

    password control added

--- Forms/change_password_form.py
+++ Forms/change_password_form.py
@@ -1,9 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Regexp
 
 
 class ChangePasswordForm(FlaskForm):
-    password = PasswordField("Введите новый пароль", validators=[DataRequired()])
+    password = PasswordField("Введите новый пароль",
+                             validators=[DataRequired(),
+                                         Regexp('^.*(?=.{8,})(?=.*[a-zA-Z])(?=.*\d).*$',
+                                                message="Пароль должен состоять минимум\
+ из 8 символов, включать цифры, прописные и строчные латинские буквы")])
     password_again = PasswordField("Повторите пароль", validators=[DataRequired()])
     submit = SubmitField("Изменить пароль")
--- Forms/register_form.py
+++ Forms/register_form.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, SubmitField
 from wtforms.fields.html5 import EmailField, TelField, DateField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Regexp
 
 
 class RegisterForm(FlaskForm):
@@ -10,6 +10,9 @@ class RegisterForm(FlaskForm):
     middle_name = StringField("Отчество")
     phone = TelField("Номер телефона", validators=[DataRequired()])
     birthdate = DateField("Дата рождения", validators=[DataRequired()])
-    password = PasswordField("Пароль", validators=[DataRequired()])
+    password = PasswordField("Пароль", validators=[DataRequired(),
+                                                   Regexp('^.*(?=.{8,})(?=.*[a-zA-Z])(?=.*\d).*$',
+                                                          message="Пароль должен состоять минимум\
+ из 8 символов, включать цифры, прописные и строчные латинские буквы")])
     password_again = PasswordField("Повторите пароль", validators=[DataRequired()])
     submit = SubmitField("Зарегистрироваться")
